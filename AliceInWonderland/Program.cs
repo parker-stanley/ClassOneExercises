5a8fdbe61bea3f0106ba15bc225685ab0567d8be
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-29 14:44:30 -0400

    first commit to push files to git

---
+++ AliceInWonderland/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AliceInWonderland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word to search for.");
+            string userInput = Console.ReadLine();
+            userInput.ToLower();
+            bool foundWord = WordFinder.Search(userInput);
+            Console.WriteLine(foundWord);
+        }
+    }
+}
